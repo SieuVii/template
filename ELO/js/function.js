1e6841589a2cb0a6be4bb45ef48c0a4b776c6422
SieuVii <huynhthanhlong753@gmail.com>
2023-05-21 17:19:00 +0700

    command

--- ELO/js/function.js
+++ ELO/js/function.js
@@ -6,12 +6,75 @@ $('#menu-close').click(function() {
     $('nav .nagivation ul').removeClass('active')
 });
 
+
+!function () {
+
+    window.Element.prototype.removeClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.remove(className);
+        }
+        return this;
+    }, window.Element.prototype.addClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.add(className);
+        }
+        return this;
+    }, window.Element.prototype.toggleClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.toggle(className);
+        }
+        return this;
+    }, window.Element.prototype.isVariableDefined = function () {
+        return !!this && typeof (this) != 'undefined' && this != null;
+    }
+}();
+
+
+
 var e = {
     init: function () {
         e.backTotop()
+        
+    },
+    isVariableDefined: function (el) {
+        return typeof !!el && (el) != 'undefined' && el != null;
+    },
+    on: function (selectors, type, listener) {
+        document.addEventListener("DOMContentLoaded", () => {
+            if (!(selectors instanceof HTMLElement) && selectors !== null) {
+                selectors = document.querySelector(selectors);
+            }
+            selectors.addEventListener(type, listener);
+        });
+    },
+    removeClass: function (selectors, className) {
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (e.isVariableDefined(selectors)) {
+            selectors.removeClass(className);
+        }
+    },
+    select: function (selectors) {
+        return document.querySelector(selectors);
     },
 /* START: 08 Back to Top */
- backTotop: function () {
+    backTotop: function () {
     var scrollpos = window.scrollY;
     var backBtn = e.select('.back-top');
     if (e.isVariableDefined(backBtn)) {
@@ -31,7 +94,8 @@ var e = {
             top: 0,
             behavior: 'smooth',
         }));
-    }
+    };
 }
-}
+};
+e.init();
 /* END: Back to Top */
